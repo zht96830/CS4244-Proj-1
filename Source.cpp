9f4b19c228aa7df223fb6411ae5ee389a67ca409
Shiyao Wang <e0325558@u.nus.edu>
2021-03-13 23:46:34 +0800

    finish CDCL main loop

--- Source.cpp
+++ Source.cpp
@@ -46,6 +46,7 @@ class CDCLSolver
     ReturnValue UnitPropagation(int decision_level);
     int pickBranchingVariable();
     void assignVariable(int literal_to_assign, int decision_level, int triggering_clause);
+    int learnConflictAndBacktrack(int decision_level);
 
 public: 
     /* intiailize class state from cin input.
@@ -63,6 +64,10 @@ ReturnValue CDCLSolver::UnitPropagation(int decision_level) {
 // returns an int with magnitude representing a variable number,
 // and sign (+/-) representing whether to assign true or false
 int CDCLSolver::pickBranchingVariable() {}
+
+int CDCLSolver::learnConflictAndBacktrack(int decision_level){
+    
+}
 
 ReturnValue CDCLSolver::runCDCL() {
     int decision_level = 0;
@@ -87,17 +92,30 @@ ReturnValue CDCLSolver::runCDCL() {
     
     // while not all variables are assigned: 
     while (num_assigned != num_variables) {
+        // pick a variable to assign
         int literal_to_assign = pickBranchingVariable();
         decision_level++;
-
         assignVariable(literal_to_assign, decision_level, -1);
 
-        while (true) {
-            // 		UnitPropResult = UnitPropLoop()
-            // 		If (UnitPropResult == UNSAT ) ConflictAnalysis&Backtrack()
-            // 		Else break // back to pickBranchingVar
+        // unit propagate | generate implication graph to check for unsat
+        up_result = UnitPropagation(decision_level);
+
+        while (up_result == ReturnValue::unsat) {
+            // Shortcircuit: If at any moment after learning some clauses and jumping back to 
+            // decision lvl 0 we realize through unit propagation the formula is unsat,
+            // return unsat
+            if (decision_level == 0) return up_result;
+            
+            // otherwise learn new clause then backtrack
+            decision_level = learnConflictAndBacktrack(decision_level);
+
+            // unit propagate for again
+            up_result = UnitPropagation(decision_level);
         }
+
+        // if unit propagation finishes without discovering UNSAT, continue to pick next variable
     }
+    // after all variables have been assigned, return SAT
     return ReturnValue::sat;
 }
 
